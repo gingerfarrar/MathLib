470fe2fd89d7da87c03cf6f74f58928422161dab
ginger.farrar <ginger.farrar@students.aie.edu.au>
2016-09-26 17:44:41 -0700

    fuck

--- MathTests/main.cpp
+++ MathTests/main.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <cstdio>
 #include "Test.h"
+#include "vec2.h"
 int main()
 {
 	assert(doNothing(0) == 5);
@@ -23,5 +24,21 @@ int main()
 	assert(PP({ 0, 0, 1, 1 }, { 0, 2, 0 }) == 1);
 	//g
 	assert(bezier(0, { 0,0,0 }, { 1,0,0 }, { 2,0,0 }, {3, 0, 0}).x == 0);
+	///////////////////////////////////////////////////////////////////////
+	//vector tests
+
+	assert((vec2{ 0,0 } == vec2{ 0,0 }));
+	assert((vec2{ 1,1 } + vec2{ -1,0 } == vec2{ 0, 1 }));
+	assert((vec2{ 1,1 } - vec2{ 1,1 } == vec2{ 0, 0 }));
+	assert((vec2{ 1,1 } * vec2{ 1,1 } == vec2{ 1, 1 }));
+	assert((vec2{ 1,1 } / vec2{ 1,1 } == vec2{ 1, 1 }));
+
+	assert((vec2{ 1,1 } * 1.f == vec2{ 1,1 }));
+	assert(( 1.f * vec2{ 1,1 } == vec2{ 1,1 }));
+	assert((vec2{ 1,1 } / 1.f == vec2{ 1,1 }));
+	assert((1.f /vec2{ 1,1 } == vec2{ 1,1 }));
+
+	assert((-vec2{ 1,1 } == vec2{ -1,-1 }));
+	assert((+vec2{ 1,1 } == vec2{ +1,+1 }));
 	return 0;
 }
